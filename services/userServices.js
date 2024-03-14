9dc6cee42b1f123303fa41a9324dac090eb4adb3
MarianaSko <maryana.skorynovych@gmail.com>
2024-03-14 16:36:57 +0200

    ability to change water rate added

--- services/userServices.js
+++ services/userServices.js
@@ -5,3 +5,5 @@ export const findUser = (filter) => User.findOne(filter);
 export const findUserById = (id) => User.findById(id);
 
 export const updateUser = (filter, data) => User.findOneAndUpdate(filter, data);
+
+export const updateWater = (id, data) => User.findByIdAndUpdate(id, data)
