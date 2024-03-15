7dec442aa4c26a0da57aa01e646be5dc65ef7937
Ariy83 <143614766+Ariy83@users.noreply.github.com>
2024-03-15 15:59:20 +0200

    edit_waterSchemaJoi

--- schemas/waterNotesSchemas.js
+++ schemas/waterNotesSchemas.js
@@ -1,25 +1,13 @@
 import Joi from "joi";
 
 const createWaterNotesSchema = Joi.object({
-  notes: Joi.array().items(
-    Joi.object({
-      time: Joi.string().required(),
-      waterVolume: Joi.number().min(1).max(5000).required(),
-    })
-  ),
-  date: Joi.date(),
-  totalWaterVolume: Joi.number(),
+  time: Joi.string().required(),
+  waterVolume: Joi.number().min(1).max(5000).required(),
 });
 
 const updateWaterNotesSchema = Joi.object({
-  notes: Joi.array().items(
-    Joi.object({
-      time: Joi.string(),
-      waterVolume: Joi.number().min(1).max(5000),
-    })
-  ),
-  date: Joi.date(),
-  totalWaterVolume: Joi.number(),
+  time: Joi.string(),
+  waterVolume: Joi.number().min(1).max(5000),
 });
 
 export default { createWaterNotesSchema, updateWaterNotesSchema };
